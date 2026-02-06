386539fe96fbb21cdfed7b0efe8186bd952f2b2c
Andrey Isenko <aisenko87@gmail.com>
2026-02-06 13:29:36 +0200

    add-text

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -2,7 +2,7 @@ import photo from "../../Photo/photo-kolya.jpg";
 const Backdrop = () => {
   return (
     <div className="back">
-      <h2 className="title3">Тут можу бути розміщена Ваша реклама </h2>
+      <h2 className="title3">Тут можe бути розміщена Ваша реклама </h2>
       <img className="img" src={photo} alt="kolya" />
     </div>
   );
